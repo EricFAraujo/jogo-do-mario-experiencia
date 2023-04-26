12b7bd026a395ed119325e482e67c9c526578f2c
Dyegho <dyeghocunha@gmail.com>
2023-04-26 09:52:48 -0300

    inclusao-do-placar-e-do-botao-restart

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 
+const botaoRestart = document.getElementById('btnRestart');
+
 const jump = () => {
     mario.classList.add('jump');
 
@@ -9,28 +11,47 @@ const jump = () => {
     }, 800);
 }
 
+let contador = 0;
+console.log(contador)
+
 const loop = setInterval(() => {
 
-const pipePosition = pipe.offsetLeft;
-const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
 
-if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 100) {
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 100) {
 
-    pipe.style.animation = 'none';
-    pipe.style.left = '${pipePosition}px';
+        pipe.style.animation = 'none';
+        pipe.style.left = '${pipePosition}px';
 
-    pipe.style.animation = 'none';
-    pipe.style.bottom = '${marioPosition}px';
+        pipe.style.animation = 'none';
+        pipe.style.bottom = '${marioPosition}px';
 
-    mario.src = './images/game-over.png';
-    mario.style.width = '80px'
-    mario.style.marginLeft = '70px'
+        mario.src = './images/game-over.png';
+        mario.style.width = '80px'
+        mario.style.marginLeft = '70px'
+        botaoRestart.classList.add('display');
 
-    clearInterval(loop);
-}
+        clearInterval(loop);
+    }
+
+    /* Adicionar isto */
+    contador++;
+    const pontos = document.getElementById('pontos')
+    pontos.innerText = contador;
+
 
 }, 10);
 
 document.addEventListener('keydown', jump);
+
+
+botaoRestart.addEventListener('click', () => {
+    location.reload();
+
+})
+
+
+
 
